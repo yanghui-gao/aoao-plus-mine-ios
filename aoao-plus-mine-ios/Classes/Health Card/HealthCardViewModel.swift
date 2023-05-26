cbceaa7732e7ca38584238259038f50067b2ff74
高炀辉 <insistgyh@foxmail.com>
2023-05-26 16:03:42 +0800

    健康证VM更改

--- aoao-plus-mine-ios/Classes/Health Card/HealthCardViewModel.swift
+++ aoao-plus-mine-ios/Classes/Health Card/HealthCardViewModel.swift
@@ -18,7 +18,7 @@ class HealthCardViewModel {
 
     let outPutResultObservable = PublishSubject<JSON>()
     
-    let outPutHealthCardResultObservable = PublishSubject<HealthContentModel>()
+//    let outPutHealthCardResultObservable = PublishSubject<HealthContentModel>()
 
     let outPutErrorObservable = PublishSubject<AAErrorModel>()
     
@@ -40,18 +40,18 @@ class HealthCardViewModel {
         /*
          获取信息
          */
-        
-        input.getHealthCardObservable.flatMapLatest { userid -> Single<Result<HealthContentModel, AAErrorModel>> in
-            let request = MultiTarget(HealthAPI.getHealthCard(userID: userid))
-            return aoaoAPIProvider.rx.aoaoRequest(request).aoaoMapObject(objectType: HealthContentModel.self)
-        }.subscribe(onNext: { res in
-            switch res {
-            case .success(let healthContent):
-                self.outPutHealthCardResultObservable.onNext(healthContent)
-            case .failure(let error):
-                self.outPutErrorObservable.onNext(error)
-            }
-        }).disposed(by: disposebag)
+//
+//        input.getHealthCardObservable.flatMapLatest { userid -> Single<Result<HealthContentModel, AAErrorModel>> in
+//            let request = MultiTarget(HealthAPI.getHealthCard(userID: userid))
+//            return aoaoAPIProvider.rx.aoaoRequest(request).aoaoMapObject(objectType: HealthContentModel.self)
+//        }.subscribe(onNext: { res in
+//            switch res {
+//            case .success(let healthContent):
+//                self.outPutHealthCardResultObservable.onNext(healthContent)
+//            case .failure(let error):
+//                self.outPutErrorObservable.onNext(error)
+//            }
+//        }).disposed(by: disposebag)
         
         /// 提交序列
         input.commitObservable.flatMapLatest{ para -> Observable<Result<[String], AAErrorModel>> in
@@ -77,7 +77,7 @@ class HealthCardViewModel {
         }).disposed(by: disposebag)
         
         self.commitObservable.flatMapLatest{ [unowned self] _ -> Single<Result<JSON, AAErrorModel>> in
-            let request = MultiTarget(HealthAPI.saveHealthCard(healthCertificate: self.healthCertificate ?? "", healthCertificateBack: self.healthCertificateBack ?? "", healthCertificateStart: self.commitPara?.healthCertificateStart ?? 0, healthCertificateEnd: self.commitPara?.healthCertificateEnd ?? 0, userid: self.commitPara?.userid ?? ""))
+            let request = MultiTarget(HealthAPI.saveHealthCard(healthCertificate: self.healthCertificate ?? "", healthCertificateBack: self.healthCertificateBack ?? "", healthCertificateStart: self.commitPara?.healthCertificateStart ?? 0, healthCertificateEnd: self.commitPara?.healthCertificateEnd ?? 0))
             return aoaoAPIProvider.rx.aoaoRequest(request).mapSwiftJSON()
         }.subscribe(onNext: { res in
             switch res {
@@ -87,24 +87,7 @@ class HealthCardViewModel {
                 self.outPutErrorObservable.onNext(error)
             }
         }).disposed(by: disposebag)
-        
-        self.outPutResultObservable.flatMapLatest{ _ -> Single<Result<UserInfoModel, AAErrorModel>> in
-            /// 用户信息
-            if let id = UserModelManager.manager.rootknightModel?.accountId {
-                let request = MultiTarget(UpLoadVaccineContentAPI.getKnightContent(accountID: id))
-                return aoaoAPIProvider.rx.aoaoRequest(request).mapObject(objectType: UserInfoModel.self)
-            } else {
-                return Single.just(Result.failure(AAErrorModel.init(zhMessage: "刷新用户信息失败")))
-            }
-           
-        }.subscribe(onNext: { res in
-            switch res {
-            case .success(let usermodel):
-                UserModelManager.manager.setUserModel(userModel: usermodel)
-            case .failure(let error):
-                self.outPutErrorObservable.onNext(error)
-            }
-        }).disposed(by: disposebag)
+
     }
 
 }
