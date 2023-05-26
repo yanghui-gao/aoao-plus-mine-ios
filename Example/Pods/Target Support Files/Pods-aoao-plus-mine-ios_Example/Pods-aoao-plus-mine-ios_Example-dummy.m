4035a6a5a6affc63d283f438428ec94514248acc
高炀辉 <insistgyh@foxmail.com>
2023-05-26 10:35:46 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-aoao-plus-mine-ios_Example/Pods-aoao-plus-mine-ios_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_aoao_plus_mine_ios_Example : NSObject
+@end
+@implementation PodsDummy_Pods_aoao_plus_mine_ios_Example
+@end
---
+++ Example/Pods/Target Support Files/aoao-plus-mine-ios/aoao-plus-mine-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_aoao_plus_mine_ios : NSObject
+@end
+@implementation PodsDummy_aoao_plus_mine_ios
+@end
