df00b90f53a67e644092a7b7f9f4d49454d65358
高炀辉 <insistgyh@foxmail.com>
2023-05-30 11:15:04 +0800

    签约路由初始化

--- aoao-plus-mine-ios/Classes/Contract/ContractRouter.swift
+++ aoao-plus-mine-ios/Classes/Contract/ContractRouter.swift
@@ -6,7 +6,28 @@
 //
 
 import UIKit
+import aoao_plus_common_ios
+import SwiftyJSON
 
-class ContractRouter: NSObject {
-
+public struct ContractRouter {
+	
+	public static func initialize() {
+		
+		/// 签约
+		navigator.register("sign".routerUrl) { url, values, context in
+			return AAMineModule.share.contractStoryboard.instantiateViewController(withIdentifier: "SignVc")
+		}
+		
+		// 查看合同
+		navigator.register("contract".routerUrl) { url, values, context in
+			let vc = ContractVC()
+			guard let para = context as? [String: Any?] else {
+				return vc
+			}
+			if let url = para["url"] as? String {
+				vc.fileUrl = url
+			}
+			return vc
+		}
+	}
 }
