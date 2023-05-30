e8f27448fba57298be1d94d2bda64f87c2d2ff14
高炀辉 <insistgyh@foxmail.com>
2023-05-30 11:08:53 +0800

    合同信息展示完成

--- aoao-plus-mine-ios/Classes/Contract/VC/ContractVC.swift
+++ aoao-plus-mine-ios/Classes/Contract/VC/ContractVC.swift
@@ -9,18 +9,9 @@ import UIKit
 import aoao_plus_common_ios
 
 class ContractVC: PreviewVc {
-
-	var url: String?
+	
     override func viewDidLoad() {
         super.viewDidLoad()
 		self.title = "电子签约"
-		setContent()
     }
-    
-	func setContent() {
-		guard let url = self.url else {
-			return
-		}
-		self.fileUrl = url
-	}
 }
--- aoao-plus-mine-ios/Classes/Contract/VM/ContractVM.swift
+++ aoao-plus-mine-ios/Classes/Contract/VM/ContractVM.swift
@@ -6,7 +6,43 @@
 //
 
 import UIKit
+import RxSwift
+import aoao_plus_net_ios
+import Moya
+import SwiftyJSON
 
-class ContractVM: NSObject {
+class ContractVM {
+	// 错误
+	let errorObservable = PublishSubject<AAErrorModel>()
+	
+	// 合同信息
+	let contractInfoObservable = PublishSubject<JSON>()
+	
+	// disposebag
+	let disposebag = DisposeBag()
+	
+	
+	init(input: Input) {
+		
+		input.getContractObservable
+			.map{ContractAPI.getContract(knightID: $0)}
+			.map{MultiTarget($0)}
+			.flatMapLatest{aoaoAPIProvider.rx.aoaoRequestToObservable($0)}
+			.mapSwiftJSON()
+			.subscribe(onNext: { res in
+				switch res{
+				case .success(let json):
+					self.contractInfoObservable.onNext(json)
+				case .failure(let error):
+					self.errorObservable.onNext(error)
+				}
+			}).disposed(by: disposebag)
+	}
+}
 
+extension ContractVM{
+	struct Input {
+		// 获取协议内容序列
+		let getContractObservable: Observable<String>
+	}
 }
