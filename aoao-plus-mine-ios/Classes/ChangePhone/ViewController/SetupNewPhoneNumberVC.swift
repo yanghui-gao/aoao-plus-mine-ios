2835521348e0d18d3ac7f826153530f4f4ff4602
高炀辉 <insistgyh@foxmail.com>
2023-06-05 16:43:43 +0800

    设置新手机号弹窗

--- aoao-plus-mine-ios/Classes/ChangePhone/ViewController/SetupNewPhoneNumberVC.swift
+++ aoao-plus-mine-ios/Classes/ChangePhone/ViewController/SetupNewPhoneNumberVC.swift
@@ -19,11 +19,8 @@ class SetupNewPhoneNumberVC: AAViewController {
 	
 	let disposeBag = DisposeBag()
 	
-	/// 获取新手机号是否被占用
-	let checkNewPhoneObservable = PublishSubject<(phone: String, idCardNum: String)>()
-	
 	/// 获取新手机号验证码
-	let getNewPhoneCodeObservable = PublishSubject<(phone: String, if_voice: Bool, event: String)>()
+	let getNewPhoneCodeObservable = PublishSubject<(phone: String, event: InputCodeType)>()
 	
 	/// 修改手机号
 	var changePhoneViewModel:ChangePhoneViewModel?
@@ -35,8 +32,26 @@ class SetupNewPhoneNumberVC: AAViewController {
 
     }
 	func setUI() {
-		
 		self.phoneTextField.becomeFirstResponder()
+	}
+	func setAlert() {
+		/// 弹窗发送验证码
+		guard let textFieldText = self.phoneTextField.text else {
+			return
+		}
+		let textFieldPhone = textFieldText.replacingOccurrences(of: " ", with: "")
+		
+		let alertVC = UIAlertController(title: "确认信息", message: "我们将发送短信验证码至：\n\(textFieldText)", preferredStyle: .alert)
+		let cancel = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+		let confim = UIAlertAction(title: "好", style: .default) { _ in
+			/// 发送新手机号验证码
+			self.getNewPhoneCodeObservable.onNext((phone: textFieldPhone, event: .changePhone_getNewCode))
+		}
+		cancel.setValue(UIColor(named: "boss_000000-60_FFFFFF-60", in: AAMineModule.share.bundle, compatibleWith: nil), forKey: "titleTextColor")
+		confim.setValue(UIColor(named: "Color_1FB1FF", in: AAMineModule.share.bundle, compatibleWith: nil), forKey: "titleTextColor")
+		alertVC.addAction(cancel)
+		alertVC.addAction(confim)
+		self.navigationController?.present(alertVC, animated: true, completion: nil)
 	}
 	func bindViewModel() {
 		/// 输入框序列
@@ -70,61 +85,23 @@ class SetupNewPhoneNumberVC: AAViewController {
 				self.view.makeToast("新手机号不能与原手机号一致!")
 				return
 			}
-			/// 身份账号
-			guard let userModel = UserModelManager.manager.userInfoModel, let idCardNnum = userModel.idCardNum else {
-				return
-			}
-			// 效验新手机号是否被占用
-			self.checkNewPhoneObservable.onNext((phone: textFieldPhone, idCardNum: idCardNnum))
+			// 验证通过弹窗
+			self.setAlert()
 			
 		}).disposed(by: disposeBag)
 		
-		let input = ChangePhoneViewModel.Input.init(getOldPhoneCode: nil, checkPhoneObservable: self.checkNewPhoneObservable, getNewPhoneCode: self.getNewPhoneCodeObservable)
+		let input = ChangePhoneViewModel.Input.init(getCodeObservable: self.getNewPhoneCodeObservable)
 		
 		self.changePhoneViewModel = ChangePhoneViewModel.init(input: input)
-		
-		// 校验手机号返回结果
-		self.changePhoneViewModel?.outPutResultObservable.subscribe(onNext: { json in
-			self.navigationController?.view.dissmissLoadingView()
-			guard let is_exists_mobile = json["is_exists_mobile"].bool else {
-				return
-			}
-			if is_exists_mobile {
-				self.view.makeToast("手机号已被占用")
-				self.view.endEditing(true)
-				return
-			} else {
-				/// 弹窗发送验证码
-				guard let textFieldText = self.phoneTextField.text else {
-					return
-				}
-				let textFieldPhone = textFieldText.replacingOccurrences(of: " ", with: "")
-				
-				let alertVC = UIAlertController(title: "确认信息", message: "我们将发送短信验证码至：\n\(textFieldText)", preferredStyle: .alert)
-				let cancel = UIAlertAction(title: "取消", style: .cancel, handler: nil)
-				let confim = UIAlertAction(title: "好", style: .default) { _ in
-					/// 发送新手机号验证码
-					self.getNewPhoneCodeObservable.onNext((phone: textFieldPhone, if_voice: false, event: "change-phone"))
-				}
-				cancel.setValue(UIColor(named: "boss_000000-60_FFFFFF-60", in: AAMineModule.share.bundle, compatibleWith: nil), forKey: "titleTextColor")
-				confim.setValue(UIColor(named: "Color_1FB1FF", in: AAMineModule.share.bundle, compatibleWith: nil), forKey: "titleTextColor")
-				alertVC.addAction(cancel)
-				alertVC.addAction(confim)
-				self.navigationController?.present(alertVC, animated: true, completion: nil)
-			}
-		}).disposed(by: disposeBag)
 		
 		/// 添加加载框
 		self.getNewPhoneCodeObservable.subscribe(onNext: { _ in
 			self.navigationController?.view.showLoadingMessage()
 		}).disposed(by: disposeBag)
 		
-		self.checkNewPhoneObservable.subscribe(onNext: { _ in
-			self.navigationController?.view.showLoadingMessage()
-		}).disposed(by: disposeBag)
 		
 		/// 获取新手机号验证码回调
-		self.changePhoneViewModel?.outPutGetNewPhoneCodeObservable.subscribe(onNext: { _ in
+		self.changePhoneViewModel?.outPutResultObservable.subscribe(onNext: { _ in
 			self.navigationController?.view.dissmissLoadingView()
 			/// 输入手机号
 			guard let textFieldText = self.phoneTextField.text else {
