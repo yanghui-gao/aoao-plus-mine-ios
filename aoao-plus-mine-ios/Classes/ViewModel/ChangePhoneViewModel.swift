82af5f968176e1f17ecf75a078bf5b746298aa3b
高炀辉 <insistgyh@foxmail.com>
2023-06-05 16:42:35 +0800

    更换手机号VM
    - 优化为获取手机号vm

--- aoao-plus-mine-ios/Classes/ViewModel/ChangePhoneViewModel.swift
+++ aoao-plus-mine-ios/Classes/ViewModel/ChangePhoneViewModel.swift
@@ -15,69 +15,33 @@ import aoao_plus_common_ios
 class ChangePhoneViewModel {
 	
 	let disposebag = DisposeBag()
-
+	// 获取验证码回调
 	let outPutResultObservable = PublishSubject<JSON>()
-	
-	let outPutGetOldPhoneCodeObservable = PublishSubject<JSON>()
-	
-	let outPutGetNewPhoneCodeObservable = PublishSubject<JSON>()
-
+	// 错误回调
 	let outPutErrorObservable = PublishSubject<AAErrorModel>()
 	
 	init(input: Input) {
 		
-		/// 获取旧手机号验证码
-		input.getOldPhoneCode?.flatMapLatest{ para -> Single<Result<JSON, AAErrorModel>> in
-			let request = MultiTarget(UpLoadVaccineContentAPI.getCode(phone: para.phone, if_voice: para.if_voice, event: para.event))
-			return aoaoAPIProvider.rx.aoaoRequest(request).mapSwiftJSON()
-			
-		}.subscribe(onNext: { res in
-			switch res {
-			case .success(let model):
-				self.outPutGetOldPhoneCodeObservable.onNext(model)
-			case .failure(let error):
-				self.outPutErrorObservable.onNext(error)
-			}
-		}).disposed(by: disposebag)
-		
-		/// 获取新手机号验证码
-		input.getNewPhoneCode?.flatMapLatest{ para -> Single<Result<JSON, AAErrorModel>> in
-			let request = MultiTarget(UpLoadVaccineContentAPI.getCode(phone: para.phone, if_voice: para.if_voice, event: para.event))
-			return aoaoAPIProvider.rx.aoaoRequest(request).mapSwiftJSON()
-			
-		}.subscribe(onNext: { res in
-			switch res {
-			case .success(let model):
-				self.outPutGetNewPhoneCodeObservable.onNext(model)
-			case .failure(let error):
-				self.outPutErrorObservable.onNext(error)
-			}
-		}).disposed(by: disposebag)
-		
-		
-		/// 验证手机号是否存在
-		input.checkPhoneObservable?.flatMapLatest{ para -> Single<Result<JSON, AAErrorModel>> in
-			let request = MultiTarget(UpLoadVaccineContentAPI.checkNewPhone(phone: para.phone, idCardNum: para.idCardNum))
-			return aoaoAPIProvider.rx.aoaoRequest(request).mapSwiftJSON()
-			
-		}.subscribe(onNext: { res in
-			switch res {
-			case .success(let model):
-				self.outPutResultObservable.onNext(model)
-			case .failure(let error):
-				self.outPutErrorObservable.onNext(error)
-			}
-		}).disposed(by: disposebag)
+		/// 修改手机号 -> 获取验证码
+		input.getCodeObservable
+			.map{UpLoadVaccineContentAPI.getCode(phone: $0.phone, event: $0.event)}
+			.map{MultiTarget($0)}
+			.flatMapLatest{ aoaoAPIProvider.rx.aoaoRequestToObservable($0)}
+			.mapSwiftJSON()
+			.subscribe(onNext: { res in
+				switch res {
+				case .success(let json):
+					self.outPutResultObservable.onNext(json)
+				case .failure(let error):
+					self.outPutErrorObservable.onNext(error)
+				}
+			}).disposed(by: disposebag)
 	}
 
 }
 extension ChangePhoneViewModel {
 	struct Input {
-		/// 获取旧手机号验证码
-		let getOldPhoneCode: Observable<(phone: String, if_voice: Bool, event: String)>?
-		/// 校验手机号是否被占用
-		let checkPhoneObservable: Observable<(phone: String, idCardNum: String)>?
-		/// 获取新手机号验证码
-		let getNewPhoneCode: Observable<(phone: String, if_voice: Bool, event: String)>?
+		/// 获取验证码
+		let getCodeObservable: Observable<(phone: String, event: InputCodeType)>
 	}
 }
